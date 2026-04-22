10d3edaffcd5a3e2f75f4b523f8483211bb61301
Havish Kotiya <havishkotiya2007@gmail.com>
2026-04-23 00:26:17 +0530

    Add files via upload

---
+++ assignment_19.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+
+struct Employee {
+    char name[50];
+    char designation[50];
+    char gender;
+    char doj[15]; 
+    float salary;
+};
+
+
+void totalEmployees(int n);
+void countGender(struct Employee e[], int n);
+void salaryAbove10000(struct Employee e[], int n);
+void asstManager(struct Employee e[], int n);
+
+int main() {
+    struct Employee e[MAX];
+    int n, i;
+
+    printf("Enter number of employees: ");
+    scanf("%d", &n);
+
+  
+    for(i = 0; i < n; i++) {
+        printf("\nEnter details of employee %d\n", i + 1);
+
+        printf("Name: ");
+        scanf("%s", e[i].name);
+
+        printf("Designation: ");
+        scanf("%s", e[i].designation);
+
+        printf("Gender (M/F): ");
+        scanf(" %c", &e[i].gender);
+
+        printf("Date of Joining (dd-mm-yyyy): ");
+        scanf("%s", e[i].doj);
+
+        printf("Salary: ");
+        scanf("%f", &e[i].salary);
+    }
+
+    totalEmployees(n);
+    countGender(e, n);
+    salaryAbove10000(e, n);
+    asstManager(e, n);
+
+    return 0;
+}
+
+
+void totalEmployees(int n) {
+    printf("\nTotal number of employees: %d\n", n);
+}
+
+
+void countGender(struct Employee e[], int n) {
+    int male = 0, female = 0, i;
+
+    for(i = 0; i < n; i++) {
+        if(e[i].gender == 'M' || e[i].gender == 'm')
+            male++;
+        else if(e[i].gender == 'F' || e[i].gender == 'f')
+            female++;
+    }
+
+    printf("\nMale employees: %d", male);
+    printf("\nFemale employees: %d\n", female);
+}
+
+
+void salaryAbove10000(struct Employee e[], int n) {
+    int i;
+
+    printf("\nEmployees with salary more than 10000:\n");
+
+    for(i = 0; i < n; i++) {
+        if(e[i].salary > 10000) {
+            printf("%s - %.2f\n", e[i].name, e[i].salary);
+        }
+    }
+}
+
+
+void asstManager(struct Employee e[], int n) {
+    int i;
+
+    printf("\nEmployees with designation Asst Manager:\n");
+
+    for(i = 0; i < n; i++) {
+        if(strcmp(e[i].designation, "AsstManager") == 0 ||
+           strcmp(e[i].designation, "Asst_Manager") == 0 ||
+           strcmp(e[i].designation, "Asst") == 0) {
+            printf("%s\n", e[i].name);
+        }
+    }
+}
